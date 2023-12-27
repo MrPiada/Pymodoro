09ee863fa2ffcc49178f9f6ddbec212b5ef74607
Federico Versari <federico.versa@gmail.com>
2023-12-27 08:01:46 +0100

    add category name in countdown display

--- src/dashboard/widgets/timer_countdown.py
+++ src/dashboard/widgets/timer_countdown.py
@@ -6,7 +6,7 @@ timer_countdown = dbc.Container([
     html.H1(id='timer-display'),
     dcc.Interval(
         id='interval-component',
-        interval=1000,  # intervallo di aggiornamento in millisecondi
+        interval=200,  # intervallo di aggiornamento in millisecondi
         n_intervals=0
     ),
     dbc.Progress(id='progress-bar', value=100, striped=True, animated=True),
