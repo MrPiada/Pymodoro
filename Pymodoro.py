a93d5d81126264cd0f2b68409c967a318c22661a
Federico Versari <federico.versa@gmail.com>
2024-01-17 17:44:32 +0100

    REFACTORING: switch to tabs instead of pages

--- Pymodoro.py
+++ Pymodoro.py
@@ -1,7 +1,6 @@
 from src.utils import *
 import dash
 from dash import html, dcc
-from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 from src.dashboard.callbacks import get_callbacks
@@ -16,7 +15,6 @@ from src.globals import *
 setupdb()
 log("INFO", "STARTUP")
 
-
 pause_categories = {
     "scacchi": 3,
     "push_up": 2,
@@ -27,7 +25,6 @@ pause_categories = {
 }
 insert_config('pippo', dict_to_string(pause_categories))
 
-
 app = dash.Dash(
     __name__,
     external_stylesheets=[
@@ -36,59 +33,18 @@ app = dash.Dash(
     title='PyModoro'
 )
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
+tabs = dbc.Tabs([
+    dbc.Tab(label='Timer', tab_id='timer', children=timer_tab),
+    dbc.Tab(label='Stats', tab_id='stats', children=stats_tab),
+    dbc.Tab(label='Config', tab_id='config', children=config_tab),
+], id='tabs', active_tab='timer')
 
-content = html.Div(id="page-content", style=CONTENT_STYLE)
-
-sidebar = html.Div([html.H2("PyModoro",
-                            className="display-4"),
-                    html.Hr(),
-                    dbc.Nav([dbc.NavLink("Timer",
-                                         href="/",
-                                         active="exact"),
-                            dbc.NavLink("Stats",
-                                        href="/stats",
-                                        active="exact"),
-                            dbc.NavLink("Config",
-                                        href="/config",
-                                        active="exact"),
-                             ],
-                            vertical=True,
-                            pills=True,
-                            ),
-                    ],
-                   style=SIDEBAR_STYLE,
-                   )
-
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    tabs,
+])
 
 get_callbacks(app)
-
-
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def render_page_content(pathname):
-    if pathname == "/":
-        Globals.POMODORI_TODAY, Globals.POMODORI_LAST_WEEK = count_past_pomodori()
-        return html.Div(children=[
-            timer_tab
-        ]),
-    elif pathname == "/stats":
-        return html.Div(children=[
-            stats_tab
-        ]),
-    elif pathname == "/config":
-        return html.Div(children=[
-            config_tab
-        ]),
-    return html.Div(
-        [
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname {pathname} was not recognised..."),
-        ],
-        className="p-3 bg-light rounded-3",
-    )
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
